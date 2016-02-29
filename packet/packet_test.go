fec313ff9328a3420a64899ccefe68eec69505e1
Oleg Yamnikov <bgeyts667@gmail.com>
2016-02-29 21:49:30 +0500

    Усложнил тесты для packet

--- packet/packet_test.go
+++ packet/packet_test.go
@@ -5,40 +5,87 @@ import (
 	"testing"
 )
 
-func TestPackageEncoding(t *testing.T) {
-	buf := bytes.NewBuffer(make([]byte, 16*1024))
-	p := &Packet{
+func TestPacket_EncodingDecoding(t *testing.T) {
+	var buf12 bytes.Buffer
+	var buf3 bytes.Buffer
+	p1 := &Packet{
 		ID: "my-packet",
 		Payload: Payload{
 			"val1": "str",
-			"val2": float64(123),
+			"val2": float64(-1),
 			"val3": true,
 		},
 	}
-	_, err := p.WriteTo(buf)
+	p2 := &Packet{
+		ID: "my-new-packet",
+		Payload: Payload{
+			"vala": "text",
+			"valb": false,
+			"valc": float64(3.14),
+		},
+	}
+	p3 := &Packet{
+		ID: "000",
+		Payload: Payload{
+			"val_i":   false,
+			"val_ii":  float64(2.71),
+			"val_iii": "line",
+		},
+	}
+
+	_, err := p1.WriteTo(&buf12)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("p1.WriteTo: %v", err)
 	}
-
-	p2, err := ReadFrom(buf)
+	_, err = p2.WriteTo(&buf12)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("p2.WriteTo: %v", err)
 	}
-	if p2 == nil {
-		t.Fatal("ReadFrom: получен нулевой указатель")
-	}
-
-	if p2.ID != p.ID {
-		t.Errorf("ID не совпали: %q получено, %q отправлено", p2.ID, p.ID)
+	_, err = p3.WriteTo(&buf3)
+	if err != nil {
+		t.Fatalf("p3.WriteTo: %v", err)
 	}
 
-	for k, v := range p.Payload {
-		v2, ok := p2.Payload[k]
-		if !ok {
-			t.Errorf("Отсутствует ключ %q", k)
+	compare := func(pa, pb *Packet) {
+		if pb.ID != pa.ID {
+			t.Errorf("ID не совпали: %q получено, %q отправлено", pb.ID, pa.ID)
 		}
-		if v2 != v {
-			t.Errorf("Значение по ключу %q не совпадает: %v получено, %v отправлено", k, v2, v)
+
+		for k, v := range pa.Payload {
+			v2, ok := pb.Payload[k]
+			if !ok {
+				t.Errorf("Отсутствует ключ %q", k)
+			}
+			if v2 != v {
+				t.Errorf("Значение по ключу %q не совпадает: %v получено, %v отправлено", k, v2, v)
+			}
 		}
 	}
+
+	r1, err := ReadFrom(&buf12)
+	if err != nil {
+		t.Fatalf("ReadFrom: %v", err)
+	}
+	if r1 == nil {
+		t.Fatal("ReadFrom: нулевой указатель")
+	}
+	compare(p1, r1)
+
+	r2, err := ReadFrom(&buf12)
+	if err != nil {
+		t.Fatalf("ReadFrom: %v", err)
+	}
+	if r2 == nil {
+		t.Fatal("ReadFrom: нулевой указатель")
+	}
+	compare(p2, r2)
+
+	r3, err := ReadFrom(&buf3)
+	if err != nil {
+		t.Fatalf("ReadFrom: %v", err)
+	}
+	if r3 == nil {
+		t.Fatal("ReadFrom: нулевой указатель")
+	}
+	compare(p3, r3)
 }
