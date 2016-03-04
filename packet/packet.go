47540dfa77254dd4416f5dc98047ce615b71323a
Oleg Kuznetsov <mr.defix@bk.ru>
2016-03-04 20:49:01 +0500

    Реализация packet.go

--- packet/packet.go
+++ packet/packet.go
@@ -1,6 +1,9 @@
 package packet
 
-import "io"
+import (
+	"encoding/json"
+	"io"
+)
 
 type Payload map[string]interface{}
 
@@ -12,13 +15,37 @@ type Packet struct {
 	Payload Payload
 }
 
+//TODO:  Json функция marchal,unmarchal; их использовааать
 // ReadFrom декодирует пакет из потока и возвращает его структуру.
 // Возвращает ошибки I/O и парсинга.
 func ReadFrom(r io.Reader) (*Packet, error) {
-	return nil, nil
+	var buf []byte
+	slice := make([]byte, 1)
+	for {
+		_, err := r.Read(slice)
+		if err != nil {
+			return nil, err
+		}
+		if slice[0] == 0 {
+			break
+		}
+		buf = append(buf, slice[0])
+	}
+	p := &Packet{}
+	err := json.Unmarshal(buf, p)
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
 }
 
 // WriteTo кодирует пакет и записывает в `w`. Реализует интерфейс `io.WriterTo`.
 func (p *Packet) WriteTo(w io.Writer) (n int64, err error) {
-	return 0, nil
+	buf, err := json.Marshal(p)
+	if err != nil {
+		return 0, err
+	}
+	buf = append(buf, 0)
+	v, err := w.Write(buf)
+	return int64(v), err
 }
